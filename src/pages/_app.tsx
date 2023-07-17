7125cdb12edb44de842ee107b728747ec4434276
Ben Thomas <bthomas@whitestar.io>
2023-07-17 12:37:43 -0400

    update reactscripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import 'setimmediate'
 import NextApp from 'next/app'
 import React from 'react'
+import { FirebaseAppProvider } from 'reactfire'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBE4oezMDApdWi9Go1nWDIiBlNIuTOeT8',
+  authDomain: 'topdeck-firebase.firebaseapp.com',
+  databaseURL: 'https://topdeck-firebase-default-rtdb.firebaseio.com',
+  projectId: 'topdeck-firebase',
+  storageBucket: 'topdeck-firebase.appspot.com',
+  messagingSenderId: '23104403186',
+  appId: '1:23104403186:web:678635c74352122cf4026b',
+}
 
 // eslint-disable-next-line
 export default function App(props: any): JSX.Element {
   // eslint-disable-next-line
   return (
-    <NextApp {...props} />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <NextApp {...props} />
+    </FirebaseAppProvider>
   )
 }
